9911e2e6edd314475d2b00cfef5135a6720c9c88
Alena <alenalswann@gmail.com>
2021-09-05 19:27:23 -0700

    updated python code

--- PyPoll.py
+++ PyPoll.py
@@ -11,11 +11,19 @@ import os
 #Assign a variable for the file to load and the path.(indirect)
 file_to_load = os.path.join("Resources", "election_results.csv")
 
-#Assign a variable for the file to load and the path.(direct)
-#file_to_load = 'Resources/election_results.csv'
-
 # Assisgn a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+#Initialize a total vote counter.
+total_votes = 0
+# Declare candidate options list
+candidate_options = []
+#Declare candidate vote count dictionary
+candidate_votes = {}
+#Declare winning candidate and winning count trackers
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 #Open the election results and read the file.
 with open(file_to_load) as election_data: 
@@ -23,25 +31,48 @@ with open(file_to_load) as election_data:
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    #read and print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count
+        total_votes += 1
+
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not  match any existing candidate...
+        if candidate_name not in candidate_options:
+            #Add candidate name to candidate options list
+            candidate_options.append(candidate_name)
+
+            # Begin tracking that candidate's vote count
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+# Determine the percentage of votes for each candidate by looping through the counts
+# Iterate through the candidate list
+for candidate_name in candidate_votes:
+    # Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    #Calculate the percentage of votes
+    vote_percentage = float(votes) / float(total_votes) * 100
+    #Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+
+    #if (votes > winning_count) and (vote_percentage > winning_percentage):
+
+
    
 
 
 
 
-#Writing in data file using open & close functions
-    # Create a filename variable to a direct or indirect path to the file.
-    #file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-    # Using the with statement open the file as a text file.
-    #outfile = open(file_to_save, "w")
-    # Write some data to the file.
-    #outfile.write("Hello World")
 
-    # Close the file
-    #outfile.close()
 
 #Writing in data file using with statements
 
